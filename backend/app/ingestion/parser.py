168cad67418ddf295b647cb228e60d7d11805835
Jamiemoorearthur <you@example.com>
2026-05-28 15:28:01 +0100

    Please see below the CV FastAPI upload for CV's all working, and the new file structure for the backend. I have also added a home route to the server.py file to make it easier to test if the server is running.

---
+++ backend/app/ingestion/parser.py
@@ -0,0 +1,13 @@
+import PyPDF2
+from io import BytesIO
+
+def extract_text_from_pdf(file_bytes: bytes) -> str:
+    pdf_reader = PyPDF2.PdfReader(BytesIO(file_bytes))
+    text = ""
+
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
+
+    return text
